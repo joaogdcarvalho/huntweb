546c7814c4de7de244a9d71b8dd726369913e21e
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-09-27 00:38:36 -0300

    armazenando no estado

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import api from '../../services/api';
 
 export default class Main extends Component {
+  // objeto de estado
+  state = {
+    products: [],
+  }
+
   // método de ciclo de vida - componentDidMount: executado assim que componente é montado em tela
   componentDidMount() {
     this.loadProducts();
@@ -12,10 +17,17 @@ export default class Main extends Component {
   loadProducts = async () => {
     const response = await api.get('/products');
 
-    console.log(response.data.docs);
+    this.setState({ products: response.data.docs });
   }
 
   render() {
-    return <h1>Hello Rocketseat</h1>;
+    // escutou que o estado foi alterado, recalculou o componente, exibindo novo valor do array
+    return (
+      <div className="product-list">
+        { this.state.products.map(product => (
+          <h2 key={product._id}>{product.title}</h2>
+        ))}
+      </div>
+    );
   }
 }
