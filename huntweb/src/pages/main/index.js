58057d6061c8e5383da4a59070afefbdc5110e74
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-09-28 22:50:19 -0300

    página anterior/próxima

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -7,6 +7,8 @@ export default class Main extends Component {
   // objeto de estado
   state = {
     products: [],
+    productInfo: {},
+    page: 1
   }
 
   // método de ciclo de vida - componentDidMount: executado assim que componente é montado em tela
@@ -16,14 +18,36 @@ export default class Main extends Component {
 
   /* função abaixo precisa ser criada com modelo 'arrow function' para que seja
   possível visualizar escopo variável 'this', não subscrevendo seu valor. */
-  loadProducts = async () => {
-    const response = await api.get('/products');
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page=${page}`);
 
-    this.setState({ products: response.data.docs });
+    const { docs, ...productInfo } = response.data;
+
+    this.setState({ products: docs, productInfo, page });
+  }
+
+  prevPage = () => {
+    const { page, productInfo } = this.state;
+
+    if (page === 1) return;
+
+    const pageNumber = page - 1;
+
+    this.loadProducts(pageNumber);
+  }
+
+  nextPage = () => {
+    const { page, productInfo } = this.state;
+
+    if (page === productInfo.pages) return;
+
+    const pageNumber = page + 1;
+
+    this.loadProducts(pageNumber);
   }
 
   render() {
-    const { products } = this.state;
+    const { products, page, productInfo } = this.state;
 
     // escutou que o estado foi alterado, recalculou o componente, exibindo novo valor do array
     return (
@@ -36,6 +60,10 @@ export default class Main extends Component {
             <a href="#">Acessar</a>
           </article>
         ))}
+        <div className="actions">
+          <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+          <button disabled={page === productInfo.pages} onClick={this.nextPage}>Próxima</button>
+        </div>
       </div>
     );
   }
